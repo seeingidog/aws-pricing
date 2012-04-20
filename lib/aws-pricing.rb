06c477a37d637fa40d6e405fb72445150f9a158b
Ian Morgan <ian@ruby-code.com>
2012-04-20 02:20:34 -0400

    Cleanup, Small doc changes

--- lib/aws-pricing.rb
+++ lib/aws-pricing.rb
@@ -4,7 +4,3 @@ dir = File.expand_path(File.join(File.dirname(__FILE__),  'aws-pricing'))
 ['base', 'ec2','s3','cloudfront', 'emr', 'rds'].each do |req|
   require File.join(dir, req)
 end
-
-module AWSPricing    
-  
-end
